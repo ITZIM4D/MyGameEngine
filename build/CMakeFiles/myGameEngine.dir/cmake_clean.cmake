32254162959f9fb8f529bd3eaca8c0d085927ff4
Jared Wilson <wilson.jared603@gmail.com>
2026-02-13 21:55:51 -0500

    Initial commit

---
+++ build/CMakeFiles/myGameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myGameEngine.dir/src/main.cpp.o"
+  "CMakeFiles/myGameEngine.dir/src/main.cpp.o.d"
+  "myGameEngine"
+  "myGameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myGameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/myGameEngine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for myGameEngine.
+# This may be replaced when dependencies are built.
